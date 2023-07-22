2c429fa8113b35cffb34676931b0846d95ac7506
Andrei Ruk <sloninc@mail.ru>
2023-07-22 22:56:54 +0300

    Основоное задание

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main()
         {
-            
+            var s = new Stack("a", "b", "c");
+            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+            var deleted = s.Pop();
+            Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
+            s.Add("d");
+            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+            s.Pop();
+            s.Pop();
+            s.Pop();
+            Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
+            //s.Pop();
+            Console.ReadLine();
         }
     }
 }
---
+++ Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11
+{
+    internal class Stack
+    {
+        private List<string> _list = new();
+        public Stack(params string[] str)
+        {
+            _list=str.ToList();
+        }
+
+        public void Add(string str)
+        {
+            _list.Add(str); 
+        }
+
+        public string Pop()
+        {
+            if (_list.Count == 0)
+                throw new Exception("Стек пустой");
+            var s=_list.Last();
+            _list.RemoveAt(_list.Count - 1);
+            return s;
+        }
+
+        public int Size
+        {
+            get { return _list.Count; }
+        }
+
+        public string Top
+        {
+            get 
+            {
+                if (_list.Count != 0)
+                    return _list[_list.Count - 1];
+                else return null;
+            }    
+        }
+    }
+}
