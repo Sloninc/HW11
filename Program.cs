e6a64098ba1d6b9404b5719a2c61670140af6586
Andrei Ruk <sloninc@mail.ru>
2023-07-22 23:05:18 +0300

    Основное задание.

--- Program.cs
+++ Program.cs
@@ -4,17 +4,25 @@
     {
         static void Main()
         {
-            var s = new Stack("a", "b", "c");
-            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
-            var deleted = s.Pop();
-            Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
-            s.Add("d");
-            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
-            s.Pop();
-            s.Pop();
-            s.Pop();
-            Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
-            //s.Pop();
+            try
+            {
+                var s = new Stack("a", "b", "c");
+                Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+                var deleted = s.Pop();
+                Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
+                s.Add("d");
+                Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+                s.Pop();
+                s.Pop();
+                s.Pop();
+                Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
+                //s.Pop();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
             Console.ReadLine();
         }
     }
